8327603badd4f50f27bd2331a73d543dc8086830
Yehonatan Ginzberg <yoyogin@gmail.com>
2016-09-18 20:58:40 +0300

    Re added statements tests, refactored configuration name to match convention

--- test/StatementsTests.java
+++ test/StatementsTests.java
@@ -13,5 +13,21 @@ public class StatementsTests {
         Report.Note("x should change state during this test")
     }
 
+    public void addRadiosWithLocal() {
+        Sphere x = new Sphere(0, 0, 0, 0);
+        int additionalRadios = 10;
 
+        x = x.addRadios(additionalRadios);
+
+        Report.Note("x should be forgotten")
+    }
+
+    public void addPointWithFieldsTest() {
+        Sphere x = new Sphere(0, 0, 0, 0);
+        int additionalZ = 10;
+
+        x = x.addPoint(1, 2, additionalZ);
+
+        Report.Note("x should be forgotten")
+    }
 }
